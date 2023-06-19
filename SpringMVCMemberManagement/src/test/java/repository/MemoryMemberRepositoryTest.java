8f4720419fa937773d8be1f6586ad70980397e05
kmindev <kangkm0324@gmailcom>
2023-06-20 00:19:47 +0900

    Test: "MemberRepository 테스트 케이스 작성"
    save, findByName, findAll, findById 메서드 모두 테스트 성공

---
+++ SpringMVCMemberManagement/src/test/java/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,82 @@
+package repository;
+
+import org.example.domain.Member;
+import org.example.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // 각 메서드 실행되고 나서 실행되는 메서드
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @DisplayName("MemberRepository save 테스트")
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+        
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @DisplayName("MemberRepository findByName 테스트")
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @DisplayName("MemberRepository findAll 테스트")
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @DisplayName("MemberRepository findById 테스트")
+    @Test
+    public void findById() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findById(member1.getId()).get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+}
