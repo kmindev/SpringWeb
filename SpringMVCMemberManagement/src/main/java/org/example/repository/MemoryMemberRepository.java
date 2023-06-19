8d569b03bc5a11643d00691f8b1e5e0f2a70891a
kmindev <kangkm0324@gmailcom>
2023-06-20 00:53:31 +0900

    Refactor: "clearStore 메서드를 오버라이딩하도록 interface에 추가"

--- SpringMVCMemberManagement/src/main/java/org/example/repository/MemoryMemberRepository.java
+++ SpringMVCMemberManagement/src/main/java/org/example/repository/MemoryMemberRepository.java
@@ -32,6 +32,7 @@ public class MemoryMemberRepository implements MemberRepository{
         return new ArrayList<>(store.values());
     }
 
+    @Override
     public void clearStore() {
         store.clear();
     }
