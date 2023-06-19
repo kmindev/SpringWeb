9b5f9690c5da4ae1e3d768d3ce50b9a8ba958926
kmindev <kangkm0324@gmailcom>
2023-06-20 00:55:12 +0900

    Test: "회원가입,  회원가입시 중복검사 테스트 케이스 작성"

---
+++ SpringMVCMemberManagement/src/test/java/org/example/service/MemberServiceTest.java
@@ -0,0 +1,64 @@
+package org.example.service;
+
+import org.example.domain.Member;
+import org.example.repository.MemberRepository;
+import org.example.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemberRepository memberRepository;
+
+    // 각 메서드가 실행되기전에 실행되는 메서드
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @DisplayName("회원가입 테스트")
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @DisplayName("회원가입 중복 테스트")
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        // then
+    }
+}
