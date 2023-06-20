e00b8fee8a405072c6cd7496827fa7c532e9cc45
kmindev <kangkm0324@gmailcom>
2023-06-20 23:30:57 +0900

    Chore: "프로젝트를 스프링 프로젝트로 변경"

---
+++ SpringMemberManagement/src/main/java/hello/hellospring/SpringMemberManagementApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMemberManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMemberManagementApplication.class, args);
+	}
+
+}
