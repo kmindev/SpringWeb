203a6b60513870abb184f3d1beaa2670650743ba
kmindev <kangkm0324@gmailcom>
2023-06-28 22:49:42 +0900

    Feat: "메모리에 저장하는 대신 h2 데이터베이스로 변경하기 위해 JDBC 버전의 CRUD 코드 추가와 MemoryMemberRepository에서 JdbcMemberRepository로 변경"

--- SpringMemberManagement/src/main/java/hello/hellospring/service/SpringConfig.java
+++ SpringMemberManagement/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,22 +1,33 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * 자바 코드로 직접 스프링 빈에 등록하는 방법
  */
 @Configuration
 public class SpringConfig {
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-//    }
+        return new JdbcMemberRepository(dataSource);
+    }
+
 }
