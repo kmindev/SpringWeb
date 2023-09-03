a9a6d156448b1dfeb0d34e70cf4dc0af07b8e261
kmindev <kangkm0324@gmail.com>
2023-09-03 22:51:54 +0900

    Feat: "login 페이지 이동 API  주소 변경, CSRF 비활성화"

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,8 +27,10 @@ public class SecurityConfig {
         );
 
         http.formLogin(form -> form
-                .loginPage("/login")
+                .loginPage("/loginForm")
         );
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
