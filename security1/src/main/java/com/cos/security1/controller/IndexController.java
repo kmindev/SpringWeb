8e62390e8d8a8eccb3f8d2c6954346469f042bc4
kmindev <kangkm0324@gmail.com>
2023-09-03 21:55:44 +0900

    Feat: "API 추가"

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // view를 리턴한다.
 public class IndexController {
@@ -11,5 +12,36 @@ public class IndexController {
         // 머스테치 (템플릿엔진) 기본경로 src/main/resources/
         // view resolver 설정: templates (prefix), .mustache (suffix) 생략 가능
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 중간에 낚아채감. - SecurityConfig 설정 후 고침
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료!";
     }
 }
