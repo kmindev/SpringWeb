70644acc15bdbfd93b7d67e359cbed404bd7f6b6
kmindev <kangkm0324@gmail.com>
2023-09-03 21:56:01 +0900

    Feat: "securiy 설정"

---
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig {
+
+    /**
+     * /user: 인증받아야 접근 가능
+     * /manger: admin, manger 권한이 있어야 접근 가능
+     * /admin: admin 권한이 있어야 접근 가능
+     */
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/user/**").authenticated()
+                .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+        );
+
+        http.formLogin(form -> form
+                .loginPage("/login")
+        );
+
+        return http.build();
+    }
+
+}
