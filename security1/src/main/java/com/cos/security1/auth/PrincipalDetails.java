7ebaf271e28f343c59701e00ed08970c0500bcce
kmindev <kangkm0324@gmail.com>
2023-09-03 23:34:43 +0900

    Feat: "스프링 시큐리티 로그인 기능 구현"

---
+++ security1/src/main/java/com/cos/security1/auth/PrincipalDetails.java
@@ -0,0 +1,70 @@
+package com.cos.security1.auth;
+
+// 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
+// 로그인 진행이 완료가 되면 시큐리티 session을 만들어준다. (Security ContextHolder)
+// 오브젝트 타입 => Authentication 타입 객체
+// Authentication 안에 User 정보가 있어야 됨.
+// User 오브젝트 타입 => UserDetails 타입 객체
+
+import com.cos.security1.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+// Security Session => Authentication => UserDetails(PrincipalDetails)
+public class PrincipalDetails implements UserDetails {
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    // 해당 User의 권한을 리턴
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole();
+            }
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getPassword();
+    }
+
+    // 계정 만료됐는지?
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정 잠겼는지?
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 비밀번호 변경 기간이 오래지났는지?
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 활성화 되었는지?
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
