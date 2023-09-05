ccd4e58f9e6039409f527abc731fa58cab608e7a
kmindev <kangkm0324@gmail.com>
2023-09-05 17:59:11 +0900

    Feat: "네이버 로그인 추가" - 네이버 로그인은 provider(google, facebook, 트우터 등)가 아니기 때문에 yml에서 provider에 등록해줘야 함.

--- security1/src/main/java/com/cos/security1/config/outh/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/cos/security1/config/outh/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package com.cos.security1.config.outh;
 import com.cos.security1.auth.PrincipalDetails;
 import com.cos.security1.config.outh.provider.FacebookUserInfo;
 import com.cos.security1.config.outh.provider.GoogleUserInfo;
+import com.cos.security1.config.outh.provider.NaverUserInfo;
 import com.cos.security1.config.outh.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -40,6 +43,9 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("페이스북 로그인 요청");
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
         } else {
             System.out.println("현재는 구글과 페이스북만 지원");
         }
@@ -54,6 +60,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         User userEntity = userRepository.findByUsername(username);
 
         if (userEntity == null) {
+            System.out.println("OAuth2 최초 로그인시");
             userEntity = User.builder()
                     .username(username)
                     .password(password)
