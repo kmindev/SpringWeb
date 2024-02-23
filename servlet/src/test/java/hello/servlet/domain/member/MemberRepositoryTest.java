c62704d4cb77cfe818b071accd3d3b4f29c06117
kmindev <kangkm0324@gmail.com>
2024-02-23 14:15:21 +0900

    서블릿으로 간단한 회원관리 예제구현
    * repsoitory 테스트 작성

---
+++ servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,48 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // Given
+        Member member = new Member("hello", 20);
+
+        // When
+        Member saveMember = memberRepository.save(member);
+
+        // Then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(saveMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    void findAll() {
+        // Given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // When
+        List<Member> result = memberRepository.findAll();
+
+        // Then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+
+}
